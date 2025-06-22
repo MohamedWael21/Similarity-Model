49ee67b9083650a30792ec7b9fa4eb360cfe14df
Mohamed wael <mohame171819@gmail.com>
2025-06-23 01:27:53 +0300

    change host of the flask to 0.0.0.0

--- main.py
+++ main.py
@@ -194,4 +194,4 @@ def health_check():
 if __name__ == '__main__':
     # Ensure the collection exists
     print("Starting Flask application...")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
